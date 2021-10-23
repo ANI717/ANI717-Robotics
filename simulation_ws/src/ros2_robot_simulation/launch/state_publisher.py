6bb8bfbbf27fac043a5a1688e5391a02cb511ba5
ANI717 <animesh.ani@live.com>
2021-10-23 05:34:52 -0600

    ani

---
+++ simulation_ws/src/ros2_robot_simulation/launch/state_publisher.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""ROS2 Robot Description Launch File.
+
+This script provides robot description. 
+
+Revision History:
+
+        2021-10-22 (Animesh): Baseline Software.
+
+Example:
+        $ colcon build && source install/setup.bash && ros2 launch ros2_robot_simulation state_publisher.py
+        $ source install/setup.bash && ros2 launch ros2_robot_simulation state_publisher.py
+        $ ros2 launch ros2_robot_simulation state_publisher.py
+
+
+"""
+
+
+#___Import Modules:
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+#___Function:
+def generate_launch_description():
+	
+    # Get the package directory
+    package_dir = get_package_share_directory('jetbot_description')
+    
+    
+    # Create launch configuration variables
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    urdf = [package_dir, '/models/urdf/', LaunchConfiguration('urdf')]
+    
+    
+    # Declare the launch arguments
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+    
+    declare_urdf_cmd = DeclareLaunchArgument(
+        'urdf',
+        default_value='jetbot.urdf')
+    
+    
+    # Specify the actions
+    start_robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state',
+        output='screen',
+        arguments=[urdf, 'robot_description:=robot_state'])
+    
+    start_joint_state_publisher_cmd = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='robot_joint_state',
+        output='screen',
+        arguments=[urdf, 'robot_description:=robot_state'])
+
+        
+    # Create the launch description and populate
+    ld = LaunchDescription()
+    
+    # Declare the launch options
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_urdf_cmd)
+    
+    # Add all actions
+    ld.add_action(start_robot_state_publisher_cmd)
+    ld.add_action(start_joint_state_publisher_cmd)
+        
+    return ld
+
+
+#                                                                              
+# end of file
+"""ANI717"""
