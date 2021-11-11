d705acc032d68b70a3a40974215da68dbe360223
Animesh Bala Ani <animesh.ani@live.com>
2021-11-11 03:16:14 -0500

    Update setup.py

--- robot_ws/src/ros2_twist_message_to_robot_motion/setup.py
+++ robot_ws/src/ros2_twist_message_to_robot_motion/setup.py
@@ -5,7 +5,7 @@ package_name = 'ros2_twist_message_to_robot_motion'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
